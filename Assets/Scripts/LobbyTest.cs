3d74c56a9dcf14c03f258accb7d5a76871283a2c
DanilR <aringai09@gmail.com>
2020-05-10 21:28:19 +0300

    Initial commit

---
+++ Assets/Scripts/LobbyTest.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class LobbyTest : MonoBehaviour {
+	public virtual void OnClientEnterLobby() 
+	{
+		Debug.Log("Client connected!");
+	}
+
+	public virtual void OnClientExitLobby()
+	{
+		Debug.Log("CLient disconnected! So sad T_T");
+	}
+}
