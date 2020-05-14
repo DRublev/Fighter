5eb99c6cf5e0169ca52706d31559f3c695cd8c19
Sir.Mefisto <niklas.sly@gmail.com>
2020-05-14 12:54:41 +0200

    It can receive smthng now
    Updated to unity 2020. Made class to get end queue string from udp port

---
+++ Assets/Scripts/UDPReciever.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Collections.Generic;
+public class UDPReciver
+{
+    //public string Ip
+    //{ get; set; }
+    public int Port
+    { get; set; }
+    private UdpClient udpReceiver;
+    private Queue<string> received;
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="ip">Not in use</param>
+    /// <param name="port"></param>
+    public UDPReciver(string ip, int port)
+    {
+        //Ip = ip;
+        Port = port;
+        udpReceiver = new UdpClient(Port);
+        received = new Queue<string>();
+    }
+    public UDPReciver() : this("127.0.0.1", 8081)
+    {
+    }
+
+    public void ReceiveStart()
+    {
+        try
+        {
+            udpReceiver.BeginReceive(new AsyncCallback(Receive), null);
+        }
+        catch(Exception e)
+        {
+            UnityEngine.Debug.LogException(e);
+        }
+    }
+    public void ReceiveEnd()
+    {
+        udpReceiver.Close();
+    }
+    //beginReceive callback
+    private void Receive(IAsyncResult result)
+    {
+        IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, Port);
+        byte[] recvd = udpReceiver.EndReceive(result, ref endPoint);
+        udpReceiver.BeginReceive(new AsyncCallback(Receive), null);
+        received.Enqueue(System.Text.Encoding.UTF8.GetString(recvd));
+    }
+}
