4c5e20592ed300f71655ec9d34cd3d7579bb37b3
Sir.Mefisto <niklas.sly@gmail.com>
2020-05-15 21:31:12 +0200

    minor changes

--- Assets/Scripts/UDPReciever.cs
+++ Assets/Scripts/UDPReciever.cs
@@ -2,15 +2,15 @@
 using System.Net.Sockets;
 using System.Net;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 public class UDPReceiver
 {
-    //public string Ip
-    //{ get; set; }
     public int Port
     { get; set; }
     private UdpClient udpReceiver;
-    private string allReceived;
+    protected byte[] readBuffer;
+    protected string stringBuffer;
     private Queue<string> received;
     /// <summary>
     /// 
@@ -18,11 +18,11 @@ public class UDPReceiver
     /// <param name="port"></param>
     public UDPReceiver(int port)
     {
-        //Ip = ip;
         Port = port;
         udpReceiver = new UdpClient(Port);
         received = new Queue<string>();
-        allReceived = string.Empty;
+        stringBuffer = string.Empty;
+        readBuffer = new byte[10240];
     }
     public UDPReceiver() : this(8081)
     {
@@ -49,22 +49,29 @@ public class UDPReceiver
         IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, Port);
         byte[] recvd = udpReceiver.EndReceive(result, ref endPoint);
         udpReceiver.BeginReceive(new AsyncCallback(Receive), null);
-        allReceived += System.Text.Encoding.UTF8.GetString(recvd);
+        this.AddToByteBuffer(recvd);
+        stringBuffer += System.Text.Encoding.UTF8.GetString(recvd);
         //checking for comlete msg
+        //parsing to string, may remove **************************************************
         string[] msgs;
-        if (JSONParser.TryParseRegex(out msgs, allReceived, @"\[\[.*?\]\]"))
+        if (JSONParser.TryParseRegex(out msgs, stringBuffer, @"\[\[.*?\]\]"))
         {
             foreach (string element in msgs)
             {
                 received.Enqueue(element);
-                allReceived = allReceived.Replace(element, string.Empty);
+                stringBuffer = stringBuffer.Replace(element, string.Empty);
             }
         }
+        //************************************************************************
         //we don't need infinite strings, save only last 256 chars
-        if (allReceived.Length >= 10240)
+        if (stringBuffer.Length >= 10240)
         {
-            allReceived = allReceived.Substring(allReceived.Length - 256);
+            stringBuffer = stringBuffer.Substring(stringBuffer.Length - 256);
         }
+    }
+    protected void AddToByteBuffer(byte[] bytes)
+    {
+        //todo
     }
     public bool GetMsg(ref List<Vector2JSON[]> result)
     {
@@ -73,4 +80,47 @@ public class UDPReceiver
         result = JSONParser.GetBonesList(received.Dequeue());
         return true;
     }    
+}
+
+public class TCPReciever : UDPReceiver
+{
+    public string Host
+    { get; set; }
+    private TcpClient tcpClient;
+    private NetworkStream netStream;
+    private int readTimeout;
+    private Thread readingThread;
+    public TCPReciever(string host, int port, int ticksPerSecond) : base(port)
+    {
+        tcpClient = new TcpClient();
+        Host = host;
+        readTimeout = 1000 / ticksPerSecond;
+    }
+    public new void ReceiveStart()
+    {
+        this.Connect();
+        readingThread = new Thread(new ThreadStart(Receiver));
+    }
+    public new void ReceiveEnd()
+    {
+
+    }
+    private async void Connect()
+    {
+        try
+        {
+            await tcpClient.ConnectAsync(Host, Port);
+            netStream = tcpClient.GetStream();
+            netStream.ReadTimeout = readTimeout;
+            Debug.Log("Connected to: " + Host + " : " + Port);
+        }
+        catch(Exception e)
+        {
+            Debug.LogException(e);
+        }
+    }
+    private void Receiver()
+    {
+        
+    }
 }
